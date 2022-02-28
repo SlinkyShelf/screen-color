e66f28d24a1c0f7c1c116108b49d86ba50dff7b5
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-02-27 18:33:57 -0600

    Added Hexcode to display

--- index.py
+++ index.py
@@ -1,9 +1,15 @@
+import math
 import pyautogui
 from PIL import Image
 from pynput import keyboard
 from pynput.mouse import Controller 
 
 mouse = Controller()
+
+hextable = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+
+def inttohex(int):
+    return hextable[math.floor(int/16)]+hextable[int%16]
 
 def on_press(key):
     try:
@@ -15,6 +21,7 @@ def on_press(key):
             im = pyautogui.screenshot(region=(mousex,mousey, 1, 1))
             pix = im.load()[0, 0]
             print(pix)
+            print("#"+inttohex(pix[0])+inttohex(pix[1])+inttohex(pix[2]))
             print("r =", pix[0])
             print("g =", pix[1])
             print("b =", pix[2])
